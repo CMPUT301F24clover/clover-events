61de3047e71afe87fc4bb883ba6e450aea5345ce
Amna Ihsan <amnaihsan@Amnas-MacBook-Air.local>
2024-11-05 15:14:42 -0700

    Added the navigation to the profile functionality, and changed the edit_profile.xml to exclude changing the picture

--- LuckyEvent/app/src/main/java/com/example/luckyevent/activities/MenuActivity.java
+++ LuckyEvent/app/src/main/java/com/example/luckyevent/activities/MenuActivity.java
@@ -1,6 +1,10 @@
 package com.example.luckyevent.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.luckyevent.R;
@@ -8,10 +12,15 @@ import com.example.luckyevent.fragments.HomePageFragment;
 import com.example.luckyevent.fragments.ScanQrFragment;
 import com.example.luckyevent.fragments.TestFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MenuActivity extends AppCompatActivity implements HomePageFragment.OnNavigateListener {
 
     BottomNavigationView bottomNavigationView;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,7 @@ public class MenuActivity extends AppCompatActivity implements HomePageFragment.
         setContentView(R.layout.menu_activity_layout);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        db = FirebaseFirestore.getInstance();
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
             if (item.getItemId() == R.id.home_item) {
@@ -29,10 +39,7 @@ public class MenuActivity extends AppCompatActivity implements HomePageFragment.
                 return true;
             }
             else if(item.getItemId() == R.id.profile_item){
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.MenuFragment, new TestFragment())
-                        .commit();
+                findProfile();
                 return true;
             }
 
@@ -53,6 +60,42 @@ public class MenuActivity extends AppCompatActivity implements HomePageFragment.
                 .beginTransaction()
                 .replace(R.id.MenuFragment, new HomePageFragment())
                 .commit();
+    }
+
+    private void findProfile() {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null){
+            String userID = firebaseUser.getUid();
+
+            db.collection("loginProfile")
+                    .document(userID)
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful() && task.getResult() != null){
+                            DocumentSnapshot doc = task.getResult();
+                            if (doc.exists()){
+                                Boolean hasUserProfile = doc.getBoolean("hasUserProfile");
+                                if (Boolean.TRUE.equals(hasUserProfile)){
+                                    Intent intent = new Intent(MenuActivity.this,ViewProfileActivity.class);
+                                    intent.putExtra("profileID",userID);
+                                    startActivity(intent);
+                                } else{
+                                    Toast.makeText(MenuActivity.this,"No profile found, please register your profile",Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(MenuActivity.this,RegisterProfileActivity.class);
+                                    intent.putExtra("profileID",userID);
+                                    startActivity(intent);
+                                }
+                            } else{
+                                Toast.makeText(MenuActivity.this,"Document does not exist",Toast.LENGTH_SHORT).show();
+                            }
+                        }else{
+                            Toast.makeText(MenuActivity.this,"Unable to find user",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }else{
+            Toast.makeText(MenuActivity.this,"No user currently logged in",Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
