5e1514abf65d960e4f8a7e6dd682233c078bc087
Oluwaseyifunmi Bamijoko <oluwaseyifunmibamijoko@gmail.com>
2024-11-05 15:13:17 -0700

    Added some test cases

--- LuckyEvent/app/src/androidTest/java/com/example/luckyevent/LoginActivityTest.java
+++ LuckyEvent/app/src/androidTest/java/com/example/luckyevent/LoginActivityTest.java
@@ -10,13 +10,23 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TextView;
+
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
 import com.example.luckyevent.activities.LoginActivity;
+import com.example.luckyevent.activities.MenuActivity;
+import com.example.luckyevent.activities.OrganizerSignInActivity;
+import com.example.luckyevent.activities.OrganizerSignUpActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -34,10 +44,66 @@ public class LoginActivityTest {
 
         onView(withId(R.id.SignInButton)).perform(click());
 
+        /* //This is not working as expected.
+        //It was supposed to check the toasts created
+
         onView(withText("Sign-in failed: Username not found"))
                 .inRoot(withDecorView(not(isRoot())))
                 .check(matches(isDisplayed()));
 
-        onView(withId(R.id.usernameInput)).perform(ViewActions.typeText(""));
+         */
+        //checks if it is still in the login activity the sign in wasn't successfull
+        ActivityScenario<LoginActivity> scenario = ActivityScenario.launch(LoginActivity.class);
+        scenario.onActivity(activity -> {
+            assertTrue(activity instanceof LoginActivity);
+        });
+
+        //type in a username that doesn't exist and try to sign in
+        onView(withId(R.id.usernameInput)).perform(ViewActions.typeText("test123"));
+        onView(withId(R.id.SignInButton)).perform(click());
+        scenario = ActivityScenario.launch(LoginActivity.class);
+        scenario.onActivity(activity -> {
+            assertTrue(activity instanceof LoginActivity);
+        });
+
+        //type in a username that exists but enter no password
+        onView(withId(R.id.usernameInput)).perform(ViewActions.typeText("JohnDoe"));
+        onView(withId(R.id.SignInButton)).perform(click());
+        scenario = ActivityScenario.launch(LoginActivity.class);
+        scenario.onActivity(activity -> {
+            assertTrue(activity instanceof LoginActivity);
+        });
+
+        //try signing with a username that is in the system but with the wrong password
+        onView(withId(R.id.usernameInput)).perform(ViewActions.typeText("JohnDoe"));
+        onView(withId(R.id.passwordInput)).perform(ViewActions.typeText("654321"));
+        onView(withId(R.id.SignInButton)).perform(click());
+        scenario = ActivityScenario.launch(LoginActivity.class);
+        scenario.onActivity(activity -> {
+            assertTrue(activity instanceof LoginActivity);
+        });
+
+        //try signing with an organizer's account
+        onView(withId(R.id.usernameInput)).perform(ViewActions.typeText("JennyDoe"));
+        onView(withId(R.id.passwordInput)).perform(ViewActions.typeText("123456"));
+        onView(withId(R.id.SignInButton)).perform(click());
+        scenario = ActivityScenario.launch(LoginActivity.class);
+        scenario.onActivity(activity -> {
+            assertTrue(activity instanceof LoginActivity);
+        });
+
+        //enter the correct username and password and verify navigation to the menu activity
+        onView(withId(R.id.usernameInput)).perform(ViewActions.typeText("JohnDoe"));
+        onView(withId(R.id.passwordInput)).perform(ViewActions.typeText("123456"));
+        onView(withId(R.id.SignInButton)).perform(click());
+        ActivityScenario<MenuActivity> menuScenario = ActivityScenario.launch(MenuActivity.class);
+        menuScenario.onActivity(activity -> {
+            assertTrue(activity instanceof MenuActivity);
+        });
+
     }
-}
+
+    }
+
+
+
