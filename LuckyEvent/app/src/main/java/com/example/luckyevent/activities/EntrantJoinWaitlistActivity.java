90b3ba2be0773e30863a8c44cac1faf302d98ed8
Amna Ihsan <amnaihsan@Amnas-MacBook-Air.local>
2024-11-05 18:39:06 -0700

    Changed the EntrantJoinWaitlistActivity to add waitlist to event collection

--- LuckyEvent/app/src/main/java/com/example/luckyevent/activities/EntrantJoinWaitlistActivity.java
+++ LuckyEvent/app/src/main/java/com/example/luckyevent/activities/EntrantJoinWaitlistActivity.java
@@ -1,33 +1,39 @@
 package com.example.luckyevent.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.luckyevent.Entrant;
 import com.example.luckyevent.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EntrantJoinWaitlistActivity extends AppCompatActivity {
     private androidx.appcompat.widget.AppCompatButton joinButton;
-    private EditText firstName;
-    private EditText lastName;
-    private EditText username;
     private FirebaseFirestore db;
+    private String eventID;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.entrant_event_screen);
         db = FirebaseFirestore.getInstance();
-        firstName = findViewById(R.id.SignUpFirstNameInput);
-        lastName = findViewById(R.id.SignUpLastNameInput);
-        username = findViewById(R.id.SignUpUsernameInput);
         joinButton = findViewById(R.id.button_join);
+        eventID = getIntent().getStringExtra("eventID");
 
         joinButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,17 +43,23 @@ public class EntrantJoinWaitlistActivity extends AppCompatActivity {
         });
     }
     private void addUserToFirestore(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String userId = user.getUid();
 
-        String FirstName = firstName.getText().toString().trim();
-        String Lastname = lastName.getText().toString().trim();
-        String Username = username.getText().toString().trim();
-
-        Entrant entrant = new Entrant(FirstName,Lastname,Username);
-        db.collection("EventWaitlist")
-                .add(entrant)
-                .addOnSuccessListener(documentReference ->
-                        Toast.makeText(EntrantJoinWaitlistActivity.this, "You have successfully joined the Waitlist!" + documentReference.getId(), Toast.LENGTH_SHORT).show())
-                .addOnFailureListener(e ->
-                        Toast.makeText(EntrantJoinWaitlistActivity.this, "Failed to join the Waitlist!" + e.getMessage(), Toast.LENGTH_SHORT).show());
+        db.collection("events")
+                .document(eventID)
+                .update("waitingList", FieldValue.arrayUnion(userId))
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(EntrantJoinWaitlistActivity.this,"You have joined the waitlist",Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(EntrantJoinWaitlistActivity.this,"Failed to join the waitlist",Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
