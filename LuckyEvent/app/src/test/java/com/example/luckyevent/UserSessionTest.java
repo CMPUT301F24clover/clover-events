5bb424b25749ae0e6d6179aec7a3e027c91681a8
Oluwaseyifunmi Bamijoko <oluwaseyifunmibamijoko@gmail.com>
2024-11-07 12:45:05 -0700

    fixed compile issues and added test cases

---
+++ LuckyEvent/app/src/test/java/com/example/luckyevent/UserSessionTest.java
@@ -0,0 +1,31 @@
+package com.example.luckyevent;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class UserSessionTest {
+    private String userId;
+    private String firstName;
+
+    @Test
+    public void testUserIdField() {
+        userId = "7VbMRJwnQNUzcs2PUkvBhkjGrtC3";
+        UserSession.getInstance().setUserId(userId);
+        assertEquals(userId, UserSession.getInstance().getUserId());
+    }
+
+    @Test
+    public void testFirstNameField() {
+        firstName = "Jerry";
+        UserSession.getInstance().setFirstName(firstName);
+        assertEquals(firstName, UserSession.getInstance().getFirstName());
+    }
+
+
+
+
+}
