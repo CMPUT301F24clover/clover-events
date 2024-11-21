fd3c0585387c847544289424d87c2e16e4c9202f
Oluwaseyifunmi Bamijoko <oluwaseyifunmibamijoko@gmail.com>
2024-11-20 17:05:12 -0700

    Pushed new files needed for the admin section

---
+++ LuckyEvent/app/src/main/java/com/example/luckyevent/fragments/AdminHomePageFragment.java
@@ -0,0 +1,24 @@
+package com.example.luckyevent.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.luckyevent.R;
+
+public class AdminHomePageFragment  extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.admin_homepage, container, false);
+
+        return rootView;
+    }
+}
